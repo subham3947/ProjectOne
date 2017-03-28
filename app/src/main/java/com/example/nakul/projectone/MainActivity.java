84c2b51a3740d3f696cfec96348b5d620e701d2d
subham3947 <maitym489@gmail.com>
2017-03-28 16:28:33 +0530

    Initial Commit

--- app/src/main/java/com/example/nakul/projectone/MainActivity.java
+++ app/src/main/java/com/example/nakul/projectone/MainActivity.java
@@ -27,9 +27,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
